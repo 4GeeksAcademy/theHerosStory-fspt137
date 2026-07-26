9ddd578a5bdd94a0c30b659258e552eb6e5bf481
MarioDiaz9 <pindadodiazmario@gmail.com>
2026-07-26 15:02:16 +0000

    User CRUD done

--- src/api/models.py
+++ src/api/models.py
@@ -6,6 +6,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(20), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
@@ -14,8 +15,9 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "is_active": self.is_active,
         }
 
     
